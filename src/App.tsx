c24b5473e933f5f561f0d87930a92bfa7f5ec6a7
Syed Fawzul Azim <syed.fawzul.azim@gmail.com>
2025-12-10 00:55:02 +0100

    added construction page for other sidebar items

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import { ClientPage } from './pages/client/ClientPage'
 import { JobsPage } from './pages/jobs/JobsPage'
 import { TemplatesPage } from './pages/templates/TemplatesPage'
 import { TemplateFieldsPage } from './pages/templates/TemplateFieldsPage'
+import { EquipmentPage } from './pages/equipment/EquipmentPage'
+import { CustomersPage } from './pages/customers/CustomersPage'
+import { SettingsPage } from './pages/settings/SettingsPage'
 import { Layout } from './layouts/Layout'
 import { AppConfiguration } from './components/AppConfiguration'
 import { GlobalModalOuterContextProvider, GlobalModal } from './components/UI/GlobalModal'
@@ -34,6 +37,9 @@ function App() {
               <Route path="/company" element={<CompanyPage />} />
               <Route path="/company/workers" element={<WorkerPage />} />
               <Route path="/company/clients" element={<ClientPage />} />
+              <Route path="/company/equipments" element={<EquipmentPage />} />
+              <Route path="/company/customers" element={<CustomersPage />} />
+              <Route path="/company/settings" element={<SettingsPage />} />
               <Route path="/company/jobs" element={<JobsPage />} />
               <Route path="/company/jobs/templates" element={<TemplatesPage />} />
               <Route path="/company/jobs/templates/:templateId/fields" element={<TemplateFieldsPage />} />
