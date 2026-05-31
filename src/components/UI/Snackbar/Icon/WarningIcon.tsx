42d9ef6c3999804e3514c34fb87016d33c339d01
Tonmoy <tonmoymashiur@gmail.com>
2026-05-31 20:32:35 +0600

    refactor: separate Snackbar icons into individual files
    Moved each SVG icon into its own file under the Icon folder with a barrel export.

---
+++ src/components/UI/Snackbar/Icon/WarningIcon.tsx
@@ -0,0 +1,8 @@
+export const WarningIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M0 20H20L10 0L0 20ZM11 17H9V15H11V17ZM11 13H9V9H11V13Z"
+      fill="currentColor"
+    />
+  </svg>
+);
